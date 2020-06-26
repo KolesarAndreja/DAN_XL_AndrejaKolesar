f60070d32340874212f64b6b016948d4c66848c8
KolesarAndreja <kolesar.andreja@gmail.com>
2020-06-26 13:06:33 +0200

    CountDown event added

--- DAN_XL_AndrejaKolesar/Computer.cs
+++ DAN_XL_AndrejaKolesar/Computer.cs
@@ -11,6 +11,10 @@ namespace DAN_XL_AndrejaKolesar
     {
         private object locker1 = new object();
         private object locker2 = new object();
+        private object locker = new object();
+        AutoResetEvent autoReset = new AutoResetEvent(false);
+        public static CountdownEvent countdown = new CountdownEvent(10);
+        public static List<int> PrintedDocuments = new List<int>();
 
         public void CreateThreadForEachComputer()
         {
@@ -28,32 +32,62 @@ namespace DAN_XL_AndrejaKolesar
                 t.Start();
             }
 
-
+            while (countdown.CurrentCount > 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (!computers[i].IsAlive)
+                    {
+                        string name = computers[i].Name;
+                        computers[i] = new Thread(() => SendRequestToPrinter(new Document()));
+                        computers[i].Name = name;
+                        computers[i].Start();
+                    }
+                }
+            }
         }
+
 
         public void SendRequestToPrinter(Document document)
         {
             Printer printer = new Printer();
             string computerName = Thread.CurrentThread.Name;
+            int computerNumber = Convert.ToInt16(computerName.Split('_')[1]);
             Console.WriteLine("{0} has sent request for printing. Document description - FORMAT:{1}  COLOR:{2}   ORIENTATION:{3}", computerName, document.Format, document.Color, document.Orientation);
+            
             if (document.Format == "A3")
             {
                 lock (locker1)
                 {
-                    printer.PrintDocument();
-                    Console.WriteLine("User of {0} can take {1} document", computerName, document.Format);
+                    if (countdown.CurrentCount > 0)
+                    {
+                        printer.PrintDocument();
+                        Console.WriteLine("User of {0} can take {1} document", computerName, document.Format);
+                    }
                 }
             }
             else
             {
                 lock (locker2)
                 {
-                    printer.PrintDocument();
-                    Console.WriteLine("User of {0} can take {1} document", computerName, document.Format);
+                    if (countdown.CurrentCount > 0)
+                    {
+                        printer.PrintDocument();
+                        Console.WriteLine("User of {0} can take {1} document", computerName, document.Format);
+                    }
                 }
-
             }
-        }
-    }
 
+            lock (locker)
+            {
+                if (!PrintedDocuments.Contains(computerNumber))
+                {
+                    countdown.Signal();
+                }
+                PrintedDocuments.Add(computerNumber);
+            }
+            Thread.Sleep(100);
+        }
+
+    }
 }
