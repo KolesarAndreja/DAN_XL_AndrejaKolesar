0bc5a60f2d72e575d2e09780f9f48a109b8297a5
KolesarAndreja <kolesar.andreja@gmail.com>
2020-06-26 10:11:03 +0200

    Creating Paleta.txt

---
+++ DAN_XL_AndrejaKolesar/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XL_AndrejaKolesar
+{
+    class Document
+    {
+        public Random random = new Random();
+        //public string Format
+        //{ get
+        //    {
+
+        //    } }
+
+        public string Color { get; set; }
+        public string Orientation { get; set; }
+    }
+}
--- DAN_XL_AndrejaKolesar/Program.cs
+++ DAN_XL_AndrejaKolesar/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,24 @@ namespace DAN_XL_AndrejaKolesar
 {
     class Program
     {
+        public static string[] documentFormat = new string[2] { "A3", "A4" };
+
+        public static void WriteColorsIntoFile()
+        {
+            string[] colors = new string[10] { "aqua", "black", "orange", "red", "blue", "purple", "green", "white", "yellow", "rose" };
+            using (StreamWriter sw = File.CreateText("Paleta.txt"))
+            {
+                foreach(string c in colors)
+                {
+                    sw.WriteLine(c);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            WriteColorsIntoFile();
+            Console.ReadKey();
         }
     }
 }
